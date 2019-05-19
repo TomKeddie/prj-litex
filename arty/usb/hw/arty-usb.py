f6426d9bfc67f316a70c2ee92ab178994fa7e752
Tom Keddie <git@bronwenandtom.com>
2019-05-19 08:32:24 -0700

    Add etherbone debug

--- arty/usb/hw/arty-usb.py
+++ arty/usb/hw/arty-usb.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import pprint
 
 # Import lxbuildenv to integrate the deps/ directory
 import lxbuildenv
@@ -22,6 +23,9 @@ from litedram.phy import s7ddrphy
 
 from liteeth.phy.mii import LiteEthPHYMII
 from liteeth.core.mac import LiteEthMAC
+from liteeth.core import LiteEthUDPIPCore
+from liteeth import common
+from liteeth.frontend.etherbone import LiteEthEtherbone
 
 from valentyusb import usbcore
 from valentyusb.usbcore import io as usbio
@@ -30,11 +34,11 @@ from valentyusb.usbcore.endpoint import EndpointType
 
 _usb_pmod = [
     ("usb", 0,
-        Subsignal("d_p", Pins("pmoda:0")),
-        Subsignal("d_n", Pins("pmoda:1")),
-        Subsignal("pullup", Pins("pmoda:2")),
-        Subsignal("led", Pins("pmoda:3")),
-        IOStandard("LVCMOS33")
+     Subsignal("d_p", Pins("pmoda:0")),
+     Subsignal("d_n", Pins("pmoda:1")),
+     Subsignal("pullup", Pins("pmoda:2")),
+     Subsignal("led", Pins("pmoda:3")),
+     IOStandard("LVCMOS33")
     ),
 ]
 
@@ -91,7 +95,7 @@ class WarmBoot(Module, AutoCSR):
         self.comb += [
             # "Reset Key" is 0xac (0b101011xx)
             do_reset.eq(self.ctrl.storage[2] & self.ctrl.storage[3] & ~self.ctrl.storage[4]
-                      & self.ctrl.storage[5] & ~self.ctrl.storage[6] & self.ctrl.storage[7])
+                        & self.ctrl.storage[5] & ~self.ctrl.storage[6] & self.ctrl.storage[7])
         ]
         do_reset.attr.add("keep")
 
@@ -104,17 +108,13 @@ class ClassicLed(gpio.GPIOOut):
 # BaseSoC ------------------------------------------------------------------------------------------
 
 class BaseSoC(SoCSDRAM):
-    interrupt_map = {
-        "usb": 3,
-    }
-    interrupt_map.update(SoCSDRAM.interrupt_map)
-    
     def __init__(self, sys_clk_freq=int(100e6), **kwargs):
         platform = arty.Platform()
         SoCSDRAM.__init__(self, platform, clk_freq=sys_clk_freq,
-                         integrated_rom_size=0x8000,
-                         integrated_sram_size=0x8000,
-                         **kwargs)
+                          integrated_rom_size=0x8000,
+                          integrated_sram_size=0x8000,
+                          cpu_variant="standard+debug",
+                          **kwargs)
 
         self.submodules.crg = _CRG(platform, sys_clk_freq)
 
@@ -131,7 +131,8 @@ class BaseSoC(SoCSDRAM):
         usb_iobuf = usbio.IoBuf(usb_pads.d_p, usb_pads.d_n, usb_pads.pullup)
         self.submodules.usb = epfifo.PerEndpointFifoInterface(usb_iobuf, debug=False)
         self.add_csr("usb")
-
+        self.add_interrupt("usb", 3)
+        
         # usb led
         self.submodules.usbled = ClassicLed(usb_pads.led)
         self.add_csr("usbled")
@@ -140,10 +141,7 @@ class BaseSoC(SoCSDRAM):
         self.submodules.reboot = WarmBoot()
         self.cpu.cpu_params.update(i_externalResetVector=self.reboot.addr.storage)
         self.add_csr("reboot")
-
-        # debug
-        # self.register_mem("vexriscv_debug", 0xf00f0000, self.cpu_or_bridge.debug_bus, 0x10)
-
+        
 
 # EthernetSoC --------------------------------------------------------------------------------------
 
@@ -160,7 +158,7 @@ class EthernetSoC(BaseSoC):
                                                self.platform.request("eth"))
         self.add_csr("ethphy")
         self.submodules.ethmac = LiteEthMAC(phy=self.ethphy, dw=32,
-            interface="wishbone", endianness=self.cpu.endianness)
+                                            interface="wishbone", endianness=self.cpu.endianness)
         self.add_wb_slave(mem_decoder(self.mem_map["ethmac"]), self.ethmac.bus)
         self.add_memory_region("ethmac", self.mem_map["ethmac"] | self.shadow_base, 0x2000)
         self.add_csr("ethmac")
@@ -175,6 +173,42 @@ class EthernetSoC(BaseSoC):
             self.ethphy.crg.cd_eth_rx.clk,
             self.ethphy.crg.cd_eth_tx.clk)
 
+# EtherboneSoC --------------------------------------------------------------------------------------
+# debug (see https://github.com/timvideos/litex-buildenv/wiki/Debugging)
+class EtherboneSoC(BaseSoC):
+    def __init__(self,
+                 mac_address=0x10e2d5000000,
+                 ip_address="192.168.1.50",
+                 **kwargs):
+        BaseSoC.__init__(self, **kwargs)
+
+        # Ethernet PHY and UDP/IP stack
+        self.submodules.ethphy = LiteEthPHYMII(self.platform.request("eth_clocks"),
+                                               self.platform.request("eth"))
+        self.add_csr("ethphy")
+        self.submodules.ethcore = LiteEthUDPIPCore(self.ethphy,
+                                                   mac_address,
+                                                   common.convert_ip(ip_address),
+                                                   self.clk_freq,
+                                                   with_icmp=True)
+
+        # Etherbone bridge
+        self.submodules.etherbone = LiteEthEtherbone(self.ethcore.udp, 20000)
+        self.add_wb_master(self.etherbone.wishbone.bus)
+
+        self.ethphy.crg.cd_eth_rx.clk.attr.add("keep")
+        self.ethphy.crg.cd_eth_tx.clk.attr.add("keep")
+        self.platform.add_period_constraint(self.crg.cd_sys.clk, 10.0)
+        self.platform.add_period_constraint(self.ethphy.crg.cd_eth_rx.clk, 40.0)
+        self.platform.add_period_constraint(self.ethphy.crg.cd_eth_tx.clk, 40.0)
+
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.ethphy.crg.cd_eth_rx.clk,
+            self.ethphy.crg.cd_eth_tx.clk)
+
+        self.register_mem("vexriscv_debug", 0xf00f0000, self.cpu.debug_bus, 0x10)
+        
 
 # Build --------------------------------------------------------------------------------------------
 
@@ -184,9 +218,12 @@ def main():
     soc_sdram_args(parser)
     parser.add_argument("--with-ethernet", action="store_true",
                         help="enable Ethernet support")
+    parser.add_argument("--without-debug", action="store_true",
+                        help="disable Ethernet debugx support")
     args = parser.parse_args()
 
-    cls = EthernetSoC if args.with_ethernet else BaseSoC
+    
+    cls = EthernetSoC if args.with_ethernet else BaseSoC if args.without_debug else EtherboneSoC
     soc = cls(**soc_sdram_argdict(args))
     builder = Builder(soc, **builder_argdict(args))
     builder.build()
