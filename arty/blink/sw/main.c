930cf5f1401246eb852d7f40900454ba97bb8a46
Tom Keddie <git@bronwenandtom.com>
2019-05-22 15:38:56 -0700

    arty/blinky : add timer events and led blink

--- arty/blink/sw/main.c
+++ arty/blink/sw/main.c
@@ -4,9 +4,11 @@
 
 #include <irq.h>
 #include <uart.h>
+#include <time.h>
 #include <generated/csr.h>
 #include <generated/mem.h>
 
+void isr(void);
 void isr(void)
 {
 	unsigned int irqs;
@@ -22,10 +24,18 @@ int main(void)
 	irq_setmask(0);
 	irq_setie(1);
 	uart_init();
-
+    time_init();
+    
 	puts("Stub firmware booting...\n");
 
-	while(1) {
+    uint32_t led = 0;
+    timer0_ev_enable_write(1);
+    while(1) {
+        if (timer0_ev_pending_read() != 0) {
+            timer0_ev_pending_write(1);
+            leds_out_write(led);
+            led ^= 1;
+        }
 	}
 
 	return 0;
