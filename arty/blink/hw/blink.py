0066cdea596c8bbcc20ec5f0b8b5e8b6a30c311d
Tom Keddie <git@bronwenandtom.com>
2019-05-22 12:21:50 -0700

    blink : remove unused code, add build of software directory

--- arty/blink/hw/blink.py
+++ arty/blink/hw/blink.py
@@ -11,21 +11,9 @@ from litex.soc.cores import gpio
 from litex.soc.integration.builder import *
 
 
-# ClassicLed ------------------------------------------------------------------------------------------
-
-class ClassicLed(gpio.GPIOOut):
-    def __init__(self, pads):
-        gpio.GPIOOut.__init__(self, pads)
-        
-        # user led 0
-        self.submodules.userled0 = ClassicLed(usb_pads.led)
-        self.add_csr("userled0")
-
 # Build --------------------------------------------------------------------------------------------
 
 def main():
-    sys_clk_freq=100e6
-
     platform = arty.Platform()
     sys_clk_freq = 1/platform.default_clk_period*1e9
     soc = soc_core.SoCCore(platform,
@@ -35,6 +23,8 @@ def main():
     led_pad = platform.request("user_led", 0)
     soc.submodules.leds = gpio.GPIOOut(led_pad)
     builder = Builder(soc)
+    builder.add_software_package("blink", src_dir="../../../../sw")
+    print(builder.software_packages)
     builder.build()
 
 
