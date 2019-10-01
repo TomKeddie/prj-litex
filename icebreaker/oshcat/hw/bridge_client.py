e82753a3f023825b08fd161db44ca0f622d2339c
Tom Keddie <git@bronwenandtom.com>
2019-10-01 15:50:10 -0700

    icebreaker/oshcat: cleanup

--- icebreaker/oshcat/hw/bridge_client.py
+++ icebreaker/oshcat/hw/bridge_client.py
@@ -4,8 +4,23 @@ from litex.tools.litex_client import RemoteClient
 wb = RemoteClient()
 wb.open()
 
-val = wb.read(0x82003000)
-print("{}\n".format(val))
+
+print("\nPWM")
+#wb.write(0x82002800, 0xff)
+#wb.write(0x82002810, 0xff)
+#wb.write(0x8200280C, 0x40)
+
+print("Enable  0x{:08X}".format(wb.read(0x82002800)))
+print("Divider 0x{:08X}".format(wb.read(0x82002804) << 8 | wb.read(0x82002808)))
+print("Period  0x{:08X}".format(wb.read(0x82002810)))
+print("Width   0x{:08X}".format(wb.read(0x8200280C)))
+print("Count   0x{:08X}".format(wb.read(0x82002814)))
+
+print("Enable  0x{:08X}".format(wb.read(0x82003000)))
+print("Divider 0x{:08X}".format(wb.read(0x82003004) << 8 | wb.read(0x82002808)))
+print("Period  0x{:08X}".format(wb.read(0x82003010)))
+print("Width   0x{:08X}".format(wb.read(0x8200300C)))
+print("Count   0x{:08X}".format(wb.read(0x82003014)))
 
 wb.close()
 
